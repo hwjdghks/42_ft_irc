e688b3331129615edad0e519db648c2d86d43c36
Jeonghwan Hwang <jhwang@c2r5s2.42seoul.kr>
2024-01-20 21:21:40 +0900

    fix logic

--- src/Server.cpp
+++ src/Server.cpp
@@ -47,7 +47,7 @@ bool Server::init(int &server_socket) const
 
 	std::memset(&server_addr, 0, sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = INADDR_ANY;
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	server_addr.sin_port = htons(this->port);
 	if (bind(server_socket, reinterpret_cast<sockaddr *>(&server_addr), sizeof(server_addr)) == -1)
 	{
