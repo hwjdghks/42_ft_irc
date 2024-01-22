e0b2d68bb99200418fc5fded472be7752c291fc3
Jeonghwan Hwang <jhwang@c3r7s2.42seoul.kr>
2024-01-22 22:43:24 +0900

    add client attribute and get/set sample code

--- inc/Client.hpp
+++ inc/Client.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <vector>
+#include <arpa/inet.h>
 
 class Client
 {
@@ -14,6 +15,10 @@ public:
 	Client &operator=(const Client &ref);
 	void setFd(const int &fd);
 	const int &getFd(void) const;
+	void setAddr(const sockaddr_in &addr);
+	sockaddr_in &getAddr(void);
+	void setAddrLen(const socklen_t &len);
+	socklen_t &getAddrLen(void);
 
 private:
 	std::string	nickname;
@@ -21,6 +26,8 @@ private:
 	std::string	hostname;
 	std::string	realname;
 	int			fd;
+	sockaddr_in	addr;
+	socklen_t	addr_len;
 	int			last_connect_time;
 };
 #endif
