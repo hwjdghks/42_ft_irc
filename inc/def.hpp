554de1baf708b74489a239eeaf5a6c574eadd0d7
Jeonghwan Hwang <jhwang@cx1r4s3.42seoul.kr>
2024-02-12 19:02:59 +0900

    change msg type to define

--- inc/def.hpp
+++ inc/def.hpp
@@ -23,10 +23,10 @@
 #define UDATA_FAIL_REGISTER 6
 
 /* 클라이언트 종료시 종료 이유를 보낼 다잉 메세지 */
-static const char *MSG_FAIL_JOIN_SERVER = "Server is full";
-static const char *MSG_FAIL_REGISTER = "Register timeout";
-static const char *MSG_FAIL_TIMEOUT = "Connection lost";
-static const char *MSG_FAIL_SYSTEM = "System call error";
+#define MSG_FAIL_JOIN_SERVER "Server is full"
+#define MSG_FAIL_REGISTER "Register timeout"
+#define MSG_FAIL_TIMEOUT "Connection lost"
+#define MSG_FAIL_SYSTEM "System call error"
 
 # define SUCCESS 1
 # define FAIL 0
